9cced7f431ffe921354e9cb79a6f8b22fe4edaae
Oren Zhu <gzhu0009@student.monash.edu>
2023-12-11 02:56:34 +1100

    finished all

--- chatgpt-clone/src/App.js
+++ chatgpt-clone/src/App.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+  const [message, setMessage] = useState(null);
+  const [value, setValue] = useState(null);
+  const [previousChat, setPreviousChat] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState(null);
+
+  const createNewChat = () => {
+    setMessage(null);
+    setValue("");
+    setCurrentTitle(null);
+  };
+
+  const handleClick = (uniqueTitles) => {
+    setCurrentTitle(uniqueTitles);
+    setMessage(null);
+    setValue("");
+  };
+
   const getMessage = async () => {
     const options = {
       method: "POST",
       body: JSON.stringify({
-        message: "Hello",
-      }), 
+        message: value,
+      }),
       headers: {
         "Content-Type": "application/json",
       },
@@ -15,28 +34,72 @@ const App = () => {
         options
       );
       const data = await response.json();
-      console.log(data);
+      setMessage(data.choices[0].message);
     } catch (error) {
       console.error(error);
     }
   };
 
+  useEffect(() => {
+    console.log(currentTitle, value, message);
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
+    }
+    if (currentTitle && value && message) {
+      setPreviousChat((prevChats) => [
+        ...prevChats,
+        {
+          title: currentTitle,
+          role: "user",
+          content: value,
+        },
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content,
+        },
+      ]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [message, currentTitle]);
+
+  // console.log(previousChat);
+  const currentChat = previousChat.filter(
+    (previousChat) => previousChat.title === currentTitle
+  );
+  const uniqueTitles = Array.from(
+    new Set(previousChat.map((previousChat) => previousChat.title))
+  );
+  // console.log(uniqueTitles);
+
   return (
     <div className="app">
       <section className="side-bar">
-        <button>+ New chat</button>
+        <button onClick={createNewChat}>+ New chat</button>
         <ul className="history">
-          <nav>
-            <p>Made by Ozzz</p>
-          </nav>
+          {uniqueTitles?.map((uniqueTitles, index) => (
+            <li key={index} onClick={() => handleClick(uniqueTitles)}>
+              {uniqueTitles}
+            </li>
+          ))}
         </ul>
+        <nav>
+          <p>Made by Ozzz</p>
+        </nav>
       </section>
       <section className="main">
-        <h1>MyGPT</h1>
-        <ul className="feed"></ul>
+        {!currentTitle && <h1>MyGPT</h1>}
+        <ul className="feed">
+          {currentChat?.map((chatMessage, index) => (
+            <li key={index}>
+              <p className="role">{chatMessage.role}</p>
+              <p>{chatMessage.content}</p>
+            </li>
+          ))}
+        </ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input />
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
             <div id="submit" onClick={getMessage}>
               →
             </div>
